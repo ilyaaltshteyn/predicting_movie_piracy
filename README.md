e7a8971ddc1621edf135234284632350e3c29309
ilya <ilyaaltshteyn@gmail.com>
2015-04-15 14:25:44 -0400

    finished basic boxofficemojo scraper, collected list of movies but not details about each movie yet

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ###Metis project 2: Predicting movie piracy
+
+To do next:
+1. Retrieve piracy data from putlocker.is
+2. Retrieve more info about each movie from boxofficemojo. Retrieve the following about each:
+ -Genre
+ -rating
+ -production budget
+ -director (later, pull length of wiki page)
+ -writer (later, pull length of wiki page)
+ -actors (later, pull length of wiki page)
+ -widest release
+3. Retrieve info from wikipedia:
+ -director fame
+ -writer fame
+ -actors' fame
+ -movie fame
+ -movie 
+4. Run descriptive and inferential stats
--- boxofficemojo_scraper.py
+++ boxofficemojo_scraper.py
@@ -1,7 +1,7 @@
 #This script scrapes movie names and various characteristics about each movie
 #from the boxofficemojo.com (BOM) website.
 
-import urllib2
+import urllib2, re
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -20,34 +20,42 @@ def pull_studio_table(studio_name, page):
     of movies listed. BOM lists a max of 100 movies per page. Returns the rows
     of the table that the movies on the BOM site are listed, as an iterable of
     bs4 tag objects."""
-
-    url = 'http://www.boxofficemojo.com/studio/chart/?page=' + str(page) + \
-    '&yr=&view=company&view2=allmovies&studio=' + studio_name + \
-    '.htm&p=.htm'
-    soup = soup_maker(url)
-    body = soup.find('div', id="body")
-    table = body.find_all('table')[2]
-    rows = table.find_all('tr')
-    # body.table
-    return rows
+    try:
+        url = 'http://www.boxofficemojo.com/studio/chart/?page=' + str(page) + \
+        '&yr=&view=company&view2=allmovies&studio=' + studio_name + \
+        '.htm&p=.htm'
+        soup = soup_maker(url)
+        body = soup.find('div', id="body")
+        table = body.find_all('table')[2]
+        rows = table.find_all('tr')
+        # body.table
+        return rows
+    except:
+        return [99, 99, 99, 99, 99, 99, 99, 99, 99]
 
 def pull_movies_from_table(table, studio_name, studio_abbrev, page):
     """Pulls apart table into rows and puts the elements that we want from
-    each row as a list. Returns a list of rows. Each row is one movie."""
+    each row as a list. Returns a list of rows. Each row is one movie.
+    In the process, also formats total_gross to be just numbers."""
 
     movies_on_1_page = []
-    try:
-        for row in table[1:101]:
+    for row in table[1:101]:
+        try:
             cols = row.find_all('td')
             rank = cols[0].string
+            link = cols[1].find('a')['href']
             title = cols[1].string
             total_gross = cols[3].string
+            total_gross = "".join(re.findall("\d+", total_gross))
             opening_date = cols[7].string
             movies_on_1_page.append([title, studio_name, studio_abbrev, page,
-            rank, total_gross, opening_date])
-    except:
-        pass
-    return movies_on_1_page
+            rank, total_gross, opening_date, link])
+        except:
+            break
+    if movies_on_1_page:
+        return movies_on_1_page
+    else:
+        return [99, 99, 99, 99, 99, 99, 99, 99, 99]    
 
 #Make a dict of studio names as keys and the shortcode that BOM uses to 
 #represent each studio as values:
@@ -64,11 +72,17 @@ studio_names = {'20th century fox' : 'fox', 'Buena vista' : 'buenavista',
 
 #Use the above functions to pull the movies for all the studios in the studio_names dictionary.
 data = pd.DataFrame()
-for studio_name, studio_abbrev in studio_names.items()[:3]:
-    rows = pull_studio_table(studio_abbrev, 1)
-    movies = pull_movies_from_table(rows, studio_name, studio_abbrev, 1)
-    data = data.append(movies, ignore_index = True)
+for studio_name, studio_abbrev in studio_names.items():
+    for pagenum in range(1,5):
+        print studio_name, pagenum
+        rows = pull_studio_table(studio_abbrev, pagenum)
+        movies = pull_movies_from_table(rows, studio_name, studio_abbrev, pagenum)
+        data = data.append(movies, ignore_index = True)
 
+data.columns = ['title', 'studio_name', 'studio_abbrev', 'page',
+    'rank', 'total_gross', 'opening_date', 'link']
 
-
+data = data.dropna()
+data.to_csv(path_or_buf = "/Users/ilya/metis/week2/project2/movie_list.csv",
+    encoding = 'utf-8')
 
