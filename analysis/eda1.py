f0ad5fd329f64a6401517de4843daf04dcb4b74e
ilya <ilyaaltshteyn@gmail.com>
2015-04-18 13:00:09 -0600

    started eda1 and ipython notebook to present it

--- analysis/eda1.py
+++ analysis/eda1.py
@@ -14,10 +14,50 @@ for col in data:
     # print data[col][data[col].notnull()].describe()
     print data[col].describe()
 
-#Plot distribution of outcome variable:
-data.version_count.hist(bins = 70, grid = False)
+#Remove any 0s in version_count, because you don't know if these come from
+#incorrect putlocker urls or from the movie actually not being up there.
+data2 = data[data.version_count != 0]
+print len(data2) #Was 3979 rows, now 3096 rows.
+
+#Now plot the distribution of the outcome variable:
+data2.version_count.hist(bins = 65, grid = False)
 sns.despine(left = True, right = True, bottom = True)
+print len(data2.version_count)
 plt.title('Distribution of version counts (the outcome variable; can also be\n\
-    thought of as a piracy index), n = 3979')
+    thought of as a piracy index), after removing version_count = 0, n = 3096')
+plt.show()
+#Cumulative distribution:
+sorted_data = np.sort(data2.version_count)
+plt.step(sorted_data, np.arange(sorted_data.size))
+plt.title('Cumulative distribution plot of version counts (the outcome\
+ variable; can also be thought \nof as a piracy index), after removing\
+ version_count = 0, n = 3096')
+plt.xlabel('Version count')
+plt.ylabel('# of movies with fewer versions')
+plt.show()
+
+
+#***----The goal below is to look at version_count by studio, but only for
+#studios that have 
+
+#Figure out how many rows each studio has:
+studios = set(data2.studio_name)
+studio_movie_counts = {}
+for studio in studios:
+    movie_count = len(data2[data2.studio_name == studio])
+    studio_movie_counts[studio] = movie_count
+
+#Remove studios that have under 50 movies:
+for i, v in studio_movie_counts.items():
+    if v < 50:
+        del studio_movie_counts[i]
+
+
+#Get basic descriptives about version_count by studio_name:
+grouped = data2.groupby(by = ['studio_name']).mean()
+version_count_by_studio = grouped['version_count']
+
+
+version_count_by_studio.plot(kind = 'bar')
 plt.show()
 
